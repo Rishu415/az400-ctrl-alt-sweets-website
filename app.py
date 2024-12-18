bb455a48f53a0c0127d38ce7de2a6e76af755bc3
mattuLA <matthew-ulasien@pluralsight.com>
2024-12-18 14:43:18 -0600

    Rebuilt website

--- app.py
+++ app.py
@@ -3,9 +3,26 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def index():    
+def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
+@app.route("/home")
+def home():
+    return render_template("Home.html")
+
+@app.route("/about")
+def about():
+    return render_template("About.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("Contact.html")
+
+@app.route('/fake_submit', methods=['POST'])
+def fake_submit():
+    # Return a simple success response
+    return "OK", 200
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-        
