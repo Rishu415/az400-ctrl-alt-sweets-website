1ab77280cadeeb2d312193cd0d535c5d3df28727
mattuLA <matthew-ulasien@pluralsight.com>
2024-12-19 11:55:00 -0600

    Added OpenTelemetry integration for App Insights

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask
+from azure.monitor.opentelemetry import configure_azure_monitor
 
 app = Flask(__name__)
+
+# configure_azure_monitor will automatically look for
+# APPLICATIONINSIGHTS_CONNECTION_STRING in the environment if not specified.
+# By enabling live metrics, you get real-time analytics.
+configure_azure_monitor(
+    enable_live_metrics=True
+    # No need to pass connection_string here as it will be taken from env var
+)
 
 @app.route("/")
 def index():
