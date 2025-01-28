886464e0ee55eefe29d4472e37fc07d1c6026d4f
mattuLA <matthew-ulasien@pluralsight.com>
2025-01-28 13:48:05 -0600

    removed workflows/updated app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 # configure_azure_monitor will automatically look for
 # APPLICATIONINSIGHTS_CONNECTION_STRING in the environment if not specified.
 # By enabling live metrics, you get real-time analytics.
-configure_azure_monitor(
-    enable_live_metrics=True
+# configure_azure_monitor(
+#     enable_live_metrics=True
     # No need to pass connection_string here as it will be taken from env var
-)
+# )
 
 @app.route("/")
 def index():
@@ -28,10 +28,7 @@ def about():
 def contact():
     return render_template("Contact.html")
 
-@app.route('/fake_submit', methods=['POST'])
-def fake_submit():
-    # Return a simple success response
-    return "OK", 200
+
 
 
 if __name__ == "__main__":
