525c0bb35fae178a93d158f37a39c2ae6a3d36ce
mattuLA <matthew-ulasien@pluralsight.com>
2025-01-29 10:33:28 -0600

    test update

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
 from flask import Flask, render_template, request
-# from azure.monitor.opentelemetry import configure_azure_monitor
+from azure.monitor.opentelemetry import configure_azure_monitor
 
 app = Flask(__name__)
 
 # configure_azure_monitor will automatically look for
 # APPLICATIONINSIGHTS_CONNECTION_STRING in the environment if not specified.
 # By enabling live metrics, you get real-time analytics.
-# configure_azure_monitor(
-#     enable_live_metrics=True
+configure_azure_monitor(
+    enable_live_metrics=True
     # No need to pass connection_string here as it will be taken from env var
-# )
+)
 
 @app.route("/")
 def index():
